c866f3d97a954d9f81815098d36280b2d173257e
wish <gjbbub05@gmail.com>
2021-03-04 20:06:51 -0900

    fix

--- src/Events/message/message.js
+++ src/Events/message/message.js
@@ -37,7 +37,7 @@ module.exports = class extends Event {
 
 		const prefix = message.content.match(mentionRegexPrefix) ?
 			message.content.match(mentionRegexPrefix)[0] : this.client.prefix;
-		if (message.content.startsWith(prefix)) return;
+		if (!message.content.startsWith(prefix)) return;
 		const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
 
 		const command = this.client.commands.get(cmd.toLowerCase()) || this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()));
